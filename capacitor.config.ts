08e4b1f87956f0410925c79defc7ce08ce0f2557
riverius <ma.escobarl@duocuc.cl>
2024-09-08 15:39:17 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Retrial Web',
+  webDir: 'www'
+};
+
+export default config;
